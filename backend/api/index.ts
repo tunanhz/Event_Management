935df8a66f274073b963a61f26b64ba23ac22e11
Dương Tuấn Anh <he180437duongtuananh@gmail.com>
2026-07-23 22:55:25 +0700

    deploy

---
+++ backend/api/index.ts
@@ -0,0 +1,12 @@
+import app from '../src/app';
+import { connectDatabase } from '../src/config/database';
+
+let isConnected = false;
+
+export default async function handler(req: any, res: any) {
+  if (!isConnected) {
+    await connectDatabase();
+    isConnected = true;
+  }
+  return app(req, res);
+}
